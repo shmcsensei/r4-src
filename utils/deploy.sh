f9a0092b1777a4465b840cb487996b0fab823060
Simon Chong <simons.mail@gmail.com>
2017-12-28 06:44:09 +1100

    Updated deployment script

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,50 +1,55 @@
 #!/usr/bin/env bash
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BUILDPATH="${SCRIPTPATH}/.."
-REPOPATH="${SCRIPTPATH}/../../r4_prod_TMP"
+BUILDPATH="$( cd "${SCRIPTPATH}/.." && pwd)"
 PWDTMP=$(pwd)
+REPOPATH="$( cd "${SCRIPTPATH}/../.." && pwd)/r4_prod_TMP"
 
 printf "\n\e[1m\e[91mDeploy\e[0m\n\n"
-#
-# echo "${PWDTMP}"
-# echo "${SCRIPTPATH}"
-# echo "${REPOPATH}"
 
-printf "\e[32mBuilding site ...\e[0m\n"
-cd "$BUILDPATH"
-npm run-script build
+echo "${PWDTMP}"
+echo "${SCRIPTPATH}"
+echo "${BUILDPATH}"
+echo "${REPOPATH}"
 
-printf "\e[32mCreating temp dir ...\e[0m\n"
+printf "\n\e[32mCreating temp dir ...\e[0m\n"
 
 mkdir -p "${REPOPATH}"
 rm -rf "${REPOPATH}"
 
-# tree "${REPOPATH}"
-
-printf "\e[32mCloning repo ...\e[0m\n"
 # Checkout production stuff
-git clone https://github.com/SimonChong/r4.git "${REPOPATH}"
+printf "\n\e[32mCloning repo ...\e[0m\n"
+git clone git@github.com:SimonChong/r4.git "${REPOPATH}"
 
 # Empty the repo
-printf "\e[32mCleaning up repo ...\e[0m\n"
-rm -rf "${REPOPATH}/[^.]*"
+printf "\n\e[32mCleaning up repo ...\e[0m\n"
+
+cd "${REPOPATH}"
+git rm -rf .
+
+printf "\n\e[32mBuilding site ...\e[0m\n"
+cd "$BUILDPATH"
+npm run-script build
+
+cp -R build/* "${REPOPATH}"
+# tree "${REPOPATH}"
+HASH=$(git rev-parse HEAD)
 
 # Add everything and commit it
-printf "\e[32mAdding new files ...\e[0m\n"
+printf "\n\e[32mAdding new files ...\e[0m\n"
 cd "${REPOPATH}"
+echo "${REPOPATH}"
 git add -A
-git commit -m "Updated from a build of the 'r4-source' repository."
-
+git commit -m "Updated from a build of the 'r4-source' repository. ${HASH}"
 
 # Push
-printf "\e[32mPushing files to the public ...\e[0m\n"
-# git push origin master
+printf "\n\e[32mPushing files to the public ...\e[0m\n"
+git push origin master
 
 # Cleanup
-printf "\e[32mCleaning up ...\e[0m\n"
+printf "\n\e[32mCleaning up ...\e[0m\n"
 rm -rf "${REPOPATH}"
 
-echo "${PWDTMP}"
+# echo "${PWDTMP}"
 cd "${PWDTMP}"
 printf "\n\e[1m\e[91mDONE!\e[0m\n\n"
